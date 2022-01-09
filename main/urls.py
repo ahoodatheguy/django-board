07899f0d395c99fd8b238b9f781b4b36576bb670
Ahadu Kebede <ahadukebede@gmail.com>
2022-01-08 19:30:10 -0500

    added routing

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.homepage, name="main-homepage")
+	path('', views.homepage, name="main-homepage"),
+	path('boards/<int:id>', views.board, name='main-board')
 ]

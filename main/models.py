c447adba5fbcfcd85b3cd7baba457d546078b66d
Ahadu Kebede <ahadukebede@gmail.com>
2022-01-08 19:23:28 -0500

    now can view boards and their messages.

--- main/models.py
+++ main/models.py
@@ -13,6 +13,7 @@ class Messages(models.Model):
 	sender = models.CharField(max_length=15)
 	subject = models.CharField(max_length=30)
 	content = models.TextField()
+	date_posted = models.DateTimeField(auto_now_add=True)
 	board = models.ForeignKey(MessageBoard, on_delete=models.CASCADE)
 
 	def __str__(self) -> str:
--- main/views.py
+++ main/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
 
+from main.models import MessageBoard
+
 
 def homepage(response):
-	context = {'title': 'Heres a title'}
+	boards = MessageBoard.objects.all()
+
+	context = {
+		'boards': boards
+        }
 	return render(response, 'main/index.html', context)
+
+
+def board(response, id):
+	board = MessageBoard.objects.get(id=id)
+	messages = board.messages_set.all()
+	context = {
+		'title': board.name,
+		'board': board,
+		'messages': messages
+        }
+	return render(response, 'main/board.html', context)
